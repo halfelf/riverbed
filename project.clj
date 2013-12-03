2fc637ce55359d2311f0bcda1892288fdeff8bef
halfelf <halfelf.ronin@gmail.com>
2013-12-03 18:32:27 +0800

    store/retrieve topology spec in mongo
    use monger and cheshire for mongo client and json encoding

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
   :aot :all
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.13"]
-                 [javax.servlet/servlet-api "2.5"]
-                 [compojure "1.1.6"]]
+                 [javax.servlet/servlet-api "2.5"] ; see ring github page
+                 [compojure "1.1.6"]
+                 [com.novemberain/monger "1.7.0-beta1"]
+                 [cheshire "5.2.0"]]
   :main oceanus.riverbed.main
   :min-lein-version "2.0.0"
   )
--- src/oceanus/riverbed/main.clj
+++ src/oceanus/riverbed/main.clj
@@ -1,5 +1,10 @@
 (ns oceanus.riverbed.main
+  (:require [cheshire.core :refer :all])
   (:require [compojure.route :as route])
+  (:require monger.core
+            monger.collection)
+  (:import [org.bson.types ObjectId]
+           [com.mongodb DB WriteConcern])
   (:use compojure.core
         compojure.handler
         org.httpkit.server))
@@ -7,21 +12,46 @@
 (defn hello-handler [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+   :body    "This is dataminr riverbed server."})
 
+(defn update-topology 
+  [req]
+  (let [body (parse-string (String. (.bytes (:body req))))
+        tpid-map {:tpid (Integer. (:tpid (:route-params req)))}]
+    (try
+      (monger.core/connect! {:host "store" :port 27017})
+      (monger.core/set-db! (monger.core/get-db "topologies"))
+      (monger.collection/update "current_running" 
+                                tpid-map 
+                                (merge tpid-map body) 
+                                :upsert true)
+      {:status  201
+       :headers {"Content-Type" "text/plain"}
+       :body    "update ok"} 
+      (catch Exception e (str "caught exception: " (.getMessage e)))
+      (finally (monger.core/disconnect!))))
+  )
+
+(defn get-topology-by-id
+  [req]
+  (try
+    (monger.core/connect! {:host "store" :port 27017})
+    (monger.core/set-db! (monger.core/get-db "topologies"))
+    (let [tpid-map {:tpid (Integer. (:tpid (:route-params req)))}
+          topo-spec (monger.collection/find-one "current_running" tpid-map)]
+      {:status  200
+       :headers {"Content-Type" "text/plain"}
+       :body    (str topo-spec)})
+    (catch Exception e (str "caught exception: " (.getMessage e)))
+    (finally (monger.core/disconnect!)))
+  )
 
 (defroutes all-routes
   (GET "/" [] hello-handler)
-;  (context "/topology/:id" []
-;           (GET "/" [] get-topology-by-id)
-;           (POST "/" [] update-topology))
+  (context "/topology/:tpid" []
+           (GET "/" [] get-topology-by-id)
+           (POST "/" [] update-topology))
   (route/not-found "404"))
 
 (run-server (api #'all-routes) {:port 8010})
-
-;(defn app [req]
-;  {:status  200
-;   :headers {"Content-Type" "text/html"}
-;   :body    "hello HTTP!"})
-;(run-server app {:port 8010})
 
