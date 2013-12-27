e1893c534f9af42c32d3b3b3b8a6ea915a6eaba3
halfelf <halfelf.ronin@gmail.com>
2013-12-27 14:43:41 +0800

    formatting for reading

--- src/oceanus/glacier/kafka_spout.clj
+++ src/oceanus/glacier/kafka_spout.clj
@@ -4,20 +4,16 @@
 (defn default-kafka-spout
   [kafka-topic serial-number]
   (str
-    (format
-    "(defspout kafka-spout-%d [\"sina_status\"]\n"
-      serial-number)
-    "  [conf context collector]\n"
-    (format 
-    "  (let [stream (atom (get-one-stream props \"%s\"))]\n" 
-      kafka-topic)
-    "    (spout\n"
-    "      (nextTuple []\n"
-    "        (doseq [raw-one-log @stream]\n"
-    "          (Thread/sleep 100)\n"
-    "          (let [one-log (-> (.message raw-one-log) String. parse-string)]\n"
-    "            (emit-spout! collector [one-log]))))\n"
-    "      (ack [id]\n"
-    "        ))))\n\n"
+    (format "(defspout kafka-spout-%d [\"sina_status\"]\n" serial-number)
+            "  [conf context collector]\n"
+    (format "  (let [stream (atom (get-one-stream props \"%s\"))]\n" kafka-topic)
+            "    (spout\n"
+            "      (nextTuple []\n"
+            "        (doseq [raw-one-log @stream]\n"
+            "          (Thread/sleep 100)\n"
+            "          (let [one-log (-> (.message raw-one-log) String. parse-string)]\n"
+            "            (emit-spout! collector [one-log]))))\n"
+            "      (ack [id]\n"
+            "        ))))\n\n"
   ))
 
