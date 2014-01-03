1f312cbec3248d72834321b0f3f5b2347c61efe1
halfelf <halfelf.ronin@gmail.com>
2014-01-03 16:18:48 +0800

    change  from keyword to string, for this key has been existed from kafka

--- src/oceanus/glacier/segmentation_bolt_maker.clj
+++ src/oceanus/glacier/segmentation_bolt_maker.clj
@@ -8,7 +8,7 @@
             "(defbolt segmentation-bolt [\"seged-weibo\"] [tuple collector]\n"
             "  (let [weibo-map (.getValue tuple 0)\n"
     (format "        words (zh-segmentation (weibo-map \"%s\"))\n" field-to-seg)
-    (format "        new-record (merge weibo-map {:seg words})]\n" field-to-seg)
+    (format "        new-record (merge weibo-map {\"seg\" words})]\n" field-to-seg)
             "    (if-not (nil? words)\n"
             "      (emit-bolt! collector [new-record] :anchor tuple))\n"
             "    (ack! collector tuple)))\n\n"))
