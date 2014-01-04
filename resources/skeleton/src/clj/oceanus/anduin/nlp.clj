5ae046bc5a09c9de5590175df6c6bacfa84ad437
halfelf <halfelf.ronin@gmail.com>
2014-01-04 15:59:45 +0800

    two new service: ads recogizing and similar text
    meantime add some comments to previous service

--- resources/skeleton/src/clj/oceanus/anduin/nlp.clj
+++ resources/skeleton/src/clj/oceanus/anduin/nlp.clj
@@ -11,16 +11,16 @@
    :conn-timeout 1000})
 
 (defn zh-segmentation [text]
-  "Segmentation for Chinese text"
-  (let [text-json   (generate-string {"text" text} {:escape-non-ascii true})
-        seg-uri     (str inner-api "/segmentation/")
-        options     (merge {:body text-json} common-headers)]
+  "Segmentation for Chinese text, return a vector of words"
+  (let [text-json (generate-string {"text" text} {:escape-non-ascii true})
+        seg-uri   (str inner-api "/segmentation/")
+        options   (merge {:body text-json} common-headers)]
     (try
       (-> (client/post seg-uri options) :body parse-string vec)
       (catch Exception e nil))))
 
 (defn zh-sentiment [text key-word]
-  "Chinese sentiment judge, return value in [-1.0, 1.0]"
+  "Chinese sentiment judge, return int in #{-1 1 0}"
   (let [text-json    (generate-string 
                        {"text" text "target" key-word} 
                        {:escape-non-ascii true})
@@ -31,4 +31,22 @@
       (-> senti-result :body (parse-string true) :Result first)
        ; `senti-result` form:  {:body "{\"Result\" [-1, -0.xxxx]}"}
       (catch Exception e nil))))
+
+(defn ads-recognize [text]
+  "Judge if the text is an ad or not, return true or false"
+  (let [text-json (generate-string {"text" text} {:escape-non-ascii true})
+        ad-uri    (str inner-api "/spam/recognize/")
+        options   (merge {:body text-json} common-headers)]
+    (try
+      (-> (client/post ad-uri options) :body parse-string)
+      (catch Exception e false))))
+
+(defn similar-judge [text id]
+  "Judge if the text is similar to anyone stored, return 0 or weibo-id now"
+  (let [text-json (generate-string {"text" text "_id" id} {:escape-non-ascii true})
+        sim-uri   (str inner-api "/similar/")
+        options   (merge {:body text-json} common-headers)]
+    (try
+      (-> (client/post sim-uri options) :body parse-string)
+      (catch Exception e 0))))
 
