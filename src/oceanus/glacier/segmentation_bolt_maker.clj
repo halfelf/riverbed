1e13b9d687f58f19d93cc0cbbae35d31bf215d8a
halfelf <halfelf.ronin@gmail.com>
2014-02-12 15:49:53 +0800

    delete many punctuations in segmentation, bug fix for deleting //@someone-postfix

--- src/oceanus/glacier/segmentation_bolt_maker.clj
+++ src/oceanus/glacier/segmentation_bolt_maker.clj
@@ -8,10 +8,10 @@
     "(defbolt segmentation-bolt [\"seged\"] [tuple collector]\n"
     "  (let [info-map (.getValue tuple 0)\n"
     "        words (zh-segmentation (info-map :lite))\n"
-    "        not-digit (vec (filter #(not (re-matches #\"\\d+\" %)) words))\n"
+    "        not-shits (vec (filter #(not (re-matches #\"[？：；“”！。，\\\"'?:;!.,_\\s\\d]+\" %)) words))\n"
     ; no digit until we can segment text like "1945年" as a whole word
-    "        new-record (merge info-map {:seg not-digit})]\n"
-    "    (if-not (nil? not-digit)\n"
+    "        new-record (merge info-map {:seg not-shits})]\n"
+    "    (if-not (empty? not-shits)\n"
     "      (emit-bolt! collector [new-record] :anchor tuple))\n"
     "    (ack! collector tuple)))\n\n"))
 
