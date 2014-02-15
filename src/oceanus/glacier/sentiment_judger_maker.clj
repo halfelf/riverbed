c3a310e0fe776ca6861374fb2f88d65e44772246
halfelf <halfelf.ronin@gmail.com>
2014-02-15 17:48:45 +0800

    sentiment field typo fix...

--- src/oceanus/glacier/sentiment_judger_maker.clj
+++ src/oceanus/glacier/sentiment_judger_maker.clj
@@ -6,7 +6,7 @@
   (str 
     "(defbolt sentiment-judger [\"sentiment-txt\"] [tuple collector]\n" 
     "  (let [info-map (.getValue tuple 0)\n"
-    "        st (zh-sentiment (info-map :txt) (ids-keys (info-map :keyid)) )\n"
+    "        st (zh-sentiment (info-map :txt) (ids-keys (info-map :kid)) )\n"
     "        new-record (merge info-map {:st st})]\n"
     "    (emit-bolt! collector [new-record] :anchor tuple)\n"
     "    (ack! collector tuple)))\n\n"
