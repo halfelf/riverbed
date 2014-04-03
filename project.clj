eca68f6607cf7111270f4242396e576a735b8023
halfelf <halfelf.ronin@gmail.com>
2014-04-03 17:23:40 +0800

    update some libs version

--- project.clj
+++ project.clj
@@ -5,15 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
   :aot :all
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.0.1"]
                  [http-kit "2.1.16"]
                  [javax.servlet/servlet-api "2.5"] ; see ring github page
-                 [clj-http "0.7.8"]
+                 [clj-http "0.9.1"]
                  [compojure "1.1.6"]
-                 [com.novemberain/langohr "1.7.0" :exclusions [com.google.guava/guava]]
-                 [com.novemberain/monger "1.7.0-beta1"]
-                 [cheshire "5.2.0"]
+                 [com.novemberain/langohr "2.8.1" :exclusions [com.google.guava/guava]]
+                 [com.novemberain/monger "1.7.0"]
+                 [cheshire "5.3.1"]
                  [org.clojure/java.jdbc "0.3.0-beta2"]
                  [mysql/mysql-connector-java "5.1.25"]
                  [org.apache.kafka/kafka_2.10 "0.8.0"]
